2d337beacce407c084bf87b292504e1ec55ef3d8
douglascustodiodesiner <94208514+douglascustodiodesigner@users.noreply.github.com>
2021-11-23 13:45:19 -0400

    Atualização 0.1.2

--- src/components/layouts/home04/Footer.jsx
+++ src/components/layouts/home04/Footer.jsx
@@ -1,45 +1,46 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom'
 class Footer extends Component {
     render() {
-        return (
-            <div className="tf-modal-content justify-content-end"> 
-                <footer id="footer" className="footer footer-s4">
+        return (          
+            <div className="section slide-personal-Intro-sixth slide-dark" id="contact">                
+                <footer id="footer" className="footer footer-s3 footer-s1-home1">
+                    <div id="footer-widget" className="footer-widget-s1 footer-widget-line bg-s1 position-relative">
                     <div className="container">
-                        <div id="footer-widget" className="footer-widget-s3 border-bottom-s2 position-relative">
-                            <h3 className="widget-title larger lt-sp06 text-white text-center">Vamos Entre em contato</h3>
-                            <div className="site-list site-list-center text-center">
-                                <Link to="#" className="bg-s1"><i className="fa fa-twitter" aria-hidden="true" /></Link>
-                                <Link to="#" className="bg-s2"><i className="fa fa-behance" aria-hidden="true" /></Link>
+                        <div className="row d-lg-flex align-items-center text-center">
+                        <div className="col-lg-12">
+                            <h3 className="widget-title larger lt-sp06">Stay Connected</h3>
+                        </div>
+                        <div className="col-lg-12">
+                            <div className="widget-info">
+                            <p className="address">Sydney, Austrlia</p>
+                            <p className="mail">hello@dolio.com</p>
+                            <p className="phone">+91 254 787 878</p>
                             </div>
                         </div>
-                    </div>
-                    <div id="bottom" className="bottom-s3">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-lg-4">
-                                    <div className="copyright lt-sp02">
-                                        © Douglas Custodio, 2021 Todos Direitos Reservados.
-                                    </div>
-                                </div>
-                                <div className="col-lg-6">
-                                    <ul className="widget-nav-menu color-s2">
-                                        <li><Link to="#">Home</Link></li>
-                                        <li><Link to="#">Portfolio</Link></li>
-                                        <li><Link to="#">About</Link></li>
-                                        <li><Link to="#">Contact</Link></li>
-                                    </ul>
-                                </div>
-                                <div className="col-lg-2">
-                                    <div className="socials-list color-s2">
-                                        <Link to="#"><i className="fa fa-facebook" aria-hidden="true" /></Link>
-                                        <Link to="#"><i className="fa fa-instagram" aria-hidden="true" /></Link>
-                                    </div>
-                                </div>
+                        <div className="col-lg-12">
+                            <div className="site-list site-list-pdl text-center">
+                            <Link to="#" className="bg-s1"><i className="fa fa-twitter" aria-hidden="true" /></Link>
+                            <Link to="#" className="bg-s2"><i className="fa fa-behance" aria-hidden="true" /></Link>
+                            <Link to="#" className="bg-s3"><span className="icon-dribble" /></Link>
                             </div>
                         </div>
+                        </div>
                     </div>
-                </footer>
+                    </div>
+                    <div id="bottom" className="bottom-s1">
+                    <div className="container d-lg-flex justify-content-between">
+                        <div className="copyright lt-sp02">
+                        © Douglas Custodio, 2021 All rights reserved.
+                        </div>
+                        <div className="socials-list color-s1">
+                        <Link to="#"><i className="fa fa-facebook" aria-hidden="true" /></Link>
+                        <Link to="#"><i className="fa fa-twitter" aria-hidden="true" /></Link>
+                        <Link to="#"><i className="fa fa-instagram" aria-hidden="true" /></Link>
+                        </div>
+                    </div>
+                    </div>
+                </footer>             
             </div>
         );
     }
